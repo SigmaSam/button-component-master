89c16b669bcd197942d3df4d11035f3df3ca0a8a
Samuel Almao Herrera <samuelalmaoherrera@gmail.com>
2022-05-22 20:25:31 -0300

    Added some functions.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,28 +1,162 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import styling from '../assets/styling.json'
+/* eslint-disable default-case */
+import React, { useState } from 'react'
+import { GithubPicker } from 'react-color'
 
 function  Buttons() {
-  const buttons = styling.map((item) => {
-    return (
-      <div style={item.buttonSquare} key={item.id}>
-        <p style={item.descriptionStyle}>{item.description}</p>
-        <button       
-          style={item.buttonStyle}
-        >
-        { item.iconLeft ? <FontAwesomeIcon icon="fa-solid fa-basket-shopping" /> : null }
-          Default
-        { item.iconRight ? <FontAwesomeIcon icon="fa-solid fa-basket-shopping" /> : null }
-        </button>
-      </div>
-    )      
+/* States Definitions*/  
+  const [defButton, setDefButton] = useState({
+     description: "<Button />",
+     style: {
+      padding:"0.5rem 1rem",
+      background: "#E0E0E0",      
+      borderRadius: "5px",
+    }
   });
-  
-  return (
+
+  const [outButton, setOutButton] = useState({
+    description: "<Button variant=”outline” />",
+    style: {
+      padding:"0.5rem 1rem",
+      background: "none",   
+      borderRadius: "5px",   
+      outline: "solid 3px #3D5AFE",
+      color: "#3D5AFE"
+    }
+  })
+
+  const [textButton, setTextButton] = useState({
+    description: "<Button variant=”text” />",
+     style: {
+      padding:"0.5rem 1rem",
+      background: "none",  
+      borderRadius: "5px",
+      color: "#3D5AFE"
+     }
+   })
+
+  /*Function Definitions*/
+  const handleChangeComplete  = (color) => {
+        setDefButton(prevState => ({
+          ...prevState,
+          style: {
+            ...prevState.style,
+            background: color.hex
+          }    
+        }));
+        setOutButton(prevState => ({
+          ...prevState,
+          style: {
+            ...prevState.style,
+            outline:`solid 3px ${color.hex}`,
+            color: color.hex
+          }    
+        }));
+        setTextButton(prevState => ({
+          ...prevState,
+          style: {
+            ...prevState.style,
+            color: color.hex
+          }    
+        }));          
+  }
+
+  const handleHover = (e) => {
+    
+    switch (e.target.name) {
+      case 'defButton':
+        if (defButton.description === "<Button />")  {
+          setDefButton(prevState => ({
+            ...prevState,
+            description: "&:hover,&:focus"
+          }))
+        } else if (defButton.description !== "<Button />") {
+          setDefButton(prevState => ({
+            ...prevState,
+            description: "<Button />"
+        }))}
+        break;
+      
+      case 'outButton':
+        if (outButton.description === "<Button variant=”outline” />")  {
+          setOutButton(prevState => ({
+            ...prevState,
+            description: "&:hover,&:focus"
+          }))
+        } else if (outButton.description !== "<Button variant=”outline” />") {
+          setOutButton(prevState => ({
+            ...prevState,
+            description: "<Button variant=”outline” />"
+        }))}
+        break;
+
+      case 'textButton':
+        if (textButton.description === "<Button variant=”text” />")  {
+          setTextButton(prevState => ({
+            ...prevState,
+            description: "&:hover,&:focus"
+          }))
+        } else if (textButton.description !== "<Button variant=”text” />") {
+          setTextButton(prevState => ({
+            ...prevState,
+            description: "<Button variant=”text” />"
+        }))}
+        break;
+    }
+}
+
+ 
+
+
+   return (
     <div>
       <p className="button-title">Buttons</p>
-      <div className="button-container">
-        {buttons}
+      <div className="button-options">
+        <div className="button-colors">
+          <label htmlFor="color">Pick a Color</label>
+          <GithubPicker
+            color={ defButton.style.background }
+            onChangeComplete={ handleChangeComplete }
+          />
+        </div>
+      </div>
+      
+
+      <div className="buttons-container">
+        <div className="button-box">
+          <p>{defButton.description}</p>
+          <button 
+            name='defButton'
+            style={defButton.style}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHover}
+          >
+            Default
+          </button>
+        </div>
+
+        <div className="button-box">
+          <p>{outButton.description}</p>
+          <button
+            style={outButton.style}
+            name='outButton'
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHover}
+          >
+            Outline
+          </button>
+        </div>
+      
+        <div className="button-box">
+          <p>{textButton.description}</p>
+          <button 
+            style={textButton.style}
+            name='textButton'
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHover}
+          >
+            Text
+          </button>
+        </div>
       </div>
     </div>    
   )
